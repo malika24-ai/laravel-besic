c951bd0dcda0e18300cae214bbcee7442657f47c
lika <malika24@mhs.unitama.ac.id>
2026-05-24 12:29:03 +0800

    pertemuan 3 Database

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,7 +1,11 @@
 <x-app>
 
     <x-slot:title>{{ $title }}</x-slot>
+    <ul class="list-group">
+        @foreach ($students as $student)
+            <li class="list-group-item">{{ $loop->iteration }} -- {{ $student->nim }} --{{ $student->name }}</li>
+        @endforeach
+    </ul>
 
-    <h1 class="fw-bold">Data Student </h1>
 </x-app>
 
